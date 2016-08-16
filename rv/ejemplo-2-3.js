08916156fc1bc6cb71d3abbd72f55d17ff09eb1a
JorgeMoralesGlez <morales.king@hotmail.com>
2016-08-16 15:15:56 -0500

    Update ejemplo-2-3.js

--- rv/ejemplo-2-3.js
+++ rv/ejemplo-2-3.js
@@ -1,4 +1,4 @@
 var miCanvas = document.getElementById("miCanvas");
 var contexto = miCanvas.getContext("2d");
 contexto.fillStyle = "red";
-contecto.fillRect (30, 30, 50, 50)
+contecto.fillRect (30, 30, 50, 50);
