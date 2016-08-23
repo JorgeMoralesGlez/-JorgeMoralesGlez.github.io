7174556efe54caba50e288bca18019f5462dce90
JorgeMoralesGlez <morales.king@hotmail.com>
2016-08-23 18:49:33 -0500

    Update Ejercicio-3.js

--- rv/Ejercicio-3.js
+++ rv/Ejercicio-3.js
@@ -11,16 +11,6 @@ forma.vertices.push( new THREE.Vector3( -1, 1, 1) );
 
 forma.faces.push( new THREE.Face3( 1, 2, 3 ) );
 forma.faces.push( new THREE.Face3( 3, 0, 1 ) );
-forma.faces.push( new THREE.Face3( 4, 5, 1 ) );
-forma.faces.push( new THREE.Face3( 1, 0 ,4 ) );
-forma.faces.push( new THREE.Face3( 6, 7, 3 ) );
-forma.faces.push( new THREE.Face3( 3, 2, 6 ) );
-forma.faces.push( new THREE.Face3( 1, 5, 6 ) );
-forma.faces.push( new THREE.Face3( 6, 2, 1 ) );
-forma.faces.push( new THREE.Face3( 5, 6, 7 ) );
-forma.faces.push( new THREE.Face3( 7, 4, 5 ) );
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
-forma.faces.push( new THREE.Face3( 4, 7, 3 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
