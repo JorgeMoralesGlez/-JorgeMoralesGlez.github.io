0d963fbcde707cf8400929f14f7ae9ac0f303c9b
JorgeMoralesGlez <morales.king@hotmail.com>
2016-08-23 19:38:30 -0500

    Update Ejercicio-3.js

--- rv/Ejercicio-3.js
+++ rv/Ejercicio-3.js
@@ -9,21 +9,23 @@ forma.vertices.push( new THREE.Vector3( 1, 1, -1) );
 forma.vertices.push( new THREE.Vector3( -1, 1, -1) );
 forma.vertices.push( new THREE.Vector3( -1, 1, 1) );
 
-forma.faces.push( new THREE.Face3( 2, 1, 0 ) );
-forma.faces.push( new THREE.Face3( 0, 3, 2 ) );
-forma.faces.push( new THREE.Face3( 1, 4, 5 ) );
-forma.faces.push( new THREE.Face3( 5, 0, 4 ) );
-forma.faces.push( new THREE.Face3( 3, 4, 7 ) );
-forma.faces.push( new THREE.Face3( 4, 0, 3 ) );
-forma.faces.push( new THREE.Face3( 6, 1, 5 ) );
+forma.faces.push( new THREE.Face3( 1, 2, 3 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 1 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
+forma.faces.push( new THREE.Face3( 4, 7, 3 ) );
+forma.faces.push( new THREE.Face3( 4, 5, 1 ) );
+forma.faces.push( new THREE.Face3( 1, 0, 4 ) );
+forma.faces.push( new THREE.Face3( 6, 7, 3 ) );
+forma.faces.push( new THREE.Face3( 3, 2, 6 ) );
+forma.faces.push( new THREE.Face3( 1, 5, 6 ) );
+forma.faces.push( new THREE.Face3( 6, 2, 1 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX(-Math.PI/4);
-malla.rotateY(Math.PI/4);
+malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add( malla );
