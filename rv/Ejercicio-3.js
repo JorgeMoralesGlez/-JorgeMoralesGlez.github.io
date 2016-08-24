644252481270072c1e0a232fc2bc03738685dff5
JorgeMoralesGlez <morales.king@hotmail.com>
2016-08-23 19:17:34 -0500

    Update Ejercicio-3.js

--- rv/Ejercicio-3.js
+++ rv/Ejercicio-3.js
@@ -11,6 +11,7 @@ forma.vertices.push( new THREE.Vector3( -1, 1, 1) );
 
 forma.faces.push( new THREE.Face3( 2, 1, 0 ) );
 forma.faces.push( new THREE.Face3( 0, 3, 2 ) );
+forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
