c6b7d0f6902f020e74060cb16d487335efc15d28
JorgeMoralesGlez <morales.king@hotmail.com>
2016-08-25 15:05:37 -0500

    Update ejemplo-figura.js

--- rv/ejemplo-figura.js
+++ rv/ejemplo-figura.js
@@ -12,9 +12,9 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camera.position.z = 100;
+camara.position.z = 100;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElemnt );
+document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
