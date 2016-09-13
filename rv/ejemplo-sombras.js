31a8618425eb20a4efc352611e1a5a7a334178f7
JorgeMoralesGlez <morales.king@hotmail.com>
2016-09-13 15:36:45 -0500

    Update ejemplo-sombras.js

--- rv/ejemplo-sombras.js
+++ rv/ejemplo-sombras.js
@@ -3,7 +3,7 @@ iluminacion.position.y = 20;
 
 var forma = new THREE.SphereGeometry( 1 );
 var material = new THREE.MeshLambertMaterial( {color: "#00cc00"} );
-var malla = new new THREE.Mesh( forma, material );
+var malla = new THREE.Mesh( forma, material );
 malla.position.y = 2;
 
 var base = new THREE.Mesh( new THREE.BoxGeometry(5, .1, 5), new THREE.MeshLabertMaterial( {color: 0xFFFFFF} ) );
