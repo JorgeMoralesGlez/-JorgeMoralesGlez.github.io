682bb2cb3d906de84739da22eb8834db3d5da2ad
JorgeMoralesGlez <morales.king@hotmail.com>
2016-09-13 15:22:15 -0500

    Update ejemplo-luzambiental.js

--- rv/ejemplo-luzambiental.js
+++ rv/ejemplo-luzambiental.js
@@ -11,7 +11,7 @@ escena.add(iluminacion);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
-var lienzo = document.getElementById("luzPuntual");
+var lienzo = document.getElementById("luzAmbiental");
 var renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true});
 
 renderizador.setSize( 600, 600);
