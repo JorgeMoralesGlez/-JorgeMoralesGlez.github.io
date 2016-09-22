77b0682237b6f0350521364631fba2332bde82f1
JorgeMoralesGlez <morales.king@hotmail.com>
2016-09-22 15:07:39 -0500

    Create ejemplo-funcion.js

---
+++ rv/ejemplo-funcion.js
@@ -0,0 +1,18 @@
+funtion init(p){
+  var malla(new THREE.BoxGeometry( p, p, p ), new THREE.MeshNormalMaterial());
+  escena = new THREE.Scene();
+  escena.add(malla);
+  camara = new THREE.PerspectiveCamera();
+  renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(700, 700);
+  document.body.appendChild(renderizador.domElement);
+  }
+  
+  var main = funtion(p){
+    renderizador.render(escena, camara);
+  }
+  
+  var escena, camara, renderizador;
+  init(l);
+  main(2);
+  
