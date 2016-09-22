4bf54f1e7908ae26b350005cb46372a275423ef7
JorgeMoralesGlez <morales.king@hotmail.com>
2016-09-22 15:27:51 -0500

    Create ejemplo-funcionloop.js

---
+++ rv/ejemplo-funcionloop.js
@@ -0,0 +1,20 @@
+function init(p){
+  var malla = new THREE.Mesh(new THREE.BoxGeometry( p, p, p ), new THREE.MeshNormalMaterial());
+  escena = new THREE.Scene();
+  escena.add(malla);
+  camara = new THREE.PerspectiveCamera();
+  renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize(700, 700);
+  document.body.appendChild(renderizador.domElement);
+  camara.position.z = 5*p;
+  }
+  
+  var loop = function(p){
+    requestAnimationFrame(loop);
+    renderizador.render(escena, camara);
+    malla.rotateY(0.01);
+  }
+  
+  var escena, camara, renderizador;
+  init(1);
+  loop();
