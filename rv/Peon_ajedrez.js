9140d7b7289b9425cf2ad007f4f5cc41f9b87a6e
JorgeMoralesGlez <morales.king@hotmail.com>
2016-11-10 15:26:20 -0600

    Create Peon_ajedrez.js

---
+++ rv/Peon_ajedrez.js
@@ -0,0 +1,12 @@
+var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
+var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
+
+
+var escena = new THREE.Scene();
+//escena.add(torreMalla);
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=50;
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
