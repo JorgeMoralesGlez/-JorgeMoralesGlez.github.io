832b94e881bc83a3d1c03eb2ee2ab2de45e725fa
JorgeMoralesGlez <morales.king@hotmail.com>
2016-11-10 15:32:58 -0600

    Update Peon_ajedrez.js

--- rv/Peon_ajedrez.js
+++ rv/Peon_ajedrez.js
@@ -1,14 +1,17 @@
 var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
 var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
+var base3Forma = new THREE.CylinderGeometry(3,4,5,20,5,false);
 
 base2Forma.translate(0,1,0);
 
 var base1Malla = new THREE.Mesh(base1Forma);
 var base2Malla= new THREE.Mesh(base2Forma);
+var base3Malla= new THREE.Mesh(base3Forma);
 
 var peonForma = new THREE.Geometry();
 peonForma.merge(base1Malla.geometry, base1Malla.matrix);
 peonForma.merge(base2Malla.geometry, base2Malla.matrix);
+peonForma.merge(base3Malla.geometry, base3Malla.matrix);
 var material= new THREE.MeshNormalMaterial();
 var peonMalla = new THREE.Mesh(peonForma, material);
 
