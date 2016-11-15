259890ac813091752386ec35ea7131e4e1d20500
JorgeMoralesGlez <morales.king@hotmail.com>
2016-11-15 15:04:59 -0600

    Update Alfil_ajedrez.js

--- rv/Alfil_ajedrez.js
+++ rv/Alfil_ajedrez.js
@@ -2,8 +2,8 @@ var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
 var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
 var troncoForma = new THREE.CylinderGeometry(2,3,20,20,20,false);
 var detalle1Forma = new THREE.CylinderGeometry(4,4,.5,20,.5,false);
-var detalle2Forma = new THREE.CylinderGeometry(3,3,.5,20,.5,false);
-var detalle3Forma = new THREE.CylinderGeometry(3,3,.5,20,.5,false);
+var detalle2Forma = new THREE.CylinderGeometry(3,3,.3,20,.3,false);
+var detalle3Forma = new THREE.CylinderGeometry(3,3,.3,20,.3,false);
 var cabezaForma = new THREE.SphereGeometry(3);
 
 base1Forma.translate(0,-9,0);
@@ -11,7 +11,7 @@ base2Forma.translate(0,-8,0);
 troncoForma.translate(0,1,0);
 cabezaForma.translate(0,13,0);
 detalle1Forma.translate(0,9,0);
-detalle2Forma.translate(0,8.5,0);
+detalle2Forma.translate(0,8.7,0);
 detalle3Forma.translate(0,9.5,0);
 
 var base1Malla = new THREE.Mesh(base1Forma);
