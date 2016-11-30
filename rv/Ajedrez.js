5d5cfb756c1562f99692d2d83a41dfdc5b693632
JorgeMoralesGlez <morales.king@hotmail.com>
2016-11-30 17:07:46 -0600

    Update Ajedrez.js

--- rv/Ajedrez.js
+++ rv/Ajedrez.js
@@ -325,20 +325,20 @@ function setup(){
   var alfilMalla3 = new THREE.Mesh(alfilForma, ceramicanegra);
   
   alfilMalla.rotateX(Math.PI/2);
-  alfilMalla.translateY(9);
+  alfilMalla.translateY(12);
   alfilMalla.translateX(20);
   
   alfilMalla1.rotateX(Math.PI/2);
-  alfilMalla1.translateY(9);
+  alfilMalla1.translateY(12);
   alfilMalla1.translateX(20);
   alfilMalla1.translateZ(-70);
   
   alfilMalla2.rotateX(Math.PI/2);
-  alfilMalla2.translateY(9);
+  alfilMalla2.translateY(12);
   alfilMalla2.translateX(50);
   
   alfilMalla3.rotateX(Math.PI/2);
-  alfilMalla3.translateY(9);
+  alfilMalla3.translateY(12);
   alfilMalla3.translateX(50);
   alfilMalla3.translateZ(-70);
   
