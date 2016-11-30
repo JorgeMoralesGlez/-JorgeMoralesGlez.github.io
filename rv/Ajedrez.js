d58daa8206207e170318edbc269d8339e8722118
JorgeMoralesGlez <morales.king@hotmail.com>
2016-11-30 17:44:12 -0600

    Update Ajedrez.js

--- rv/Ajedrez.js
+++ rv/Ajedrez.js
@@ -345,12 +345,12 @@ function setup(){
   //Peon
   var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
   var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
-  var troncoForma = new THREE.CylinderGeometry(2,4,10,20,10,false);
-  var cabezaForma = new THREE.SphereGeometry(3);
+  var troncoForma = new THREE.CylinderGeometry(1.5,3,7,7,7,false);
+  var cabezaForma = new THREE.SphereGeometry(2);
 
   base2Forma.translate(0,1,0);
   troncoForma.translate(0,5,0);
-  cabezaForma.translate(0,12,0);
+  cabezaForma.translate(0,9.5,0);
 
   var base1Malla = new THREE.Mesh(base1Forma);
   var base2Malla= new THREE.Mesh(base2Forma);
