3080c5c06eb4502a0a90e921401d9b5284aee15d
JorgeMoralesGlez <morales.king@hotmail.com>
2016-12-01 17:20:55 -0600

    Update Ajedrez.js

--- rv/Ajedrez.js
+++ rv/Ajedrez.js
@@ -693,7 +693,7 @@ function loop(){
   }
   else if(cuyo==4){
     alert(auxx)
-    if(auxx!=0){
+    if(auxx=!0){
       valor[0][0].position.x=seleccionadorMalla.position.x;
       valor[0][0].position.y=seleccionadorMalla.position.y;
       var a = valor[seleccionadorMalla.position.x][seleccionadorMalla.position.y];
