adfaf7a53dc368a0ecb56ab1616decd4bf24899d
JorgeMoralesGlez <morales.king@hotmail.com>
2016-12-07 21:38:53 -0600

    Update Ajedrez_prueba.js

--- rv/Ajedrez_prueba.js
+++ rv/Ajedrez_prueba.js
@@ -295,6 +295,26 @@ function Caballoplan(x0, y0, xf, yf, side){
 			}
 		} 
   }
+  else if(div4==2 && div3==-1)
+  {
+	x0=parseInt(piezaActual.position.x);
+	        if(xf!=x0){
+			piezaActual.position.x-=1;
+		}else if(xf==x0){
+			y0=parseInt(piezaActual.position.y);
+			if(yf!=y0)
+			{
+				piezaActual.position.y+=1;	
+			}else if(yf==y0){
+				valor[xfs][yfs]= piezaActual;
+				valor[x0s][y0s]= piezaPosterior;
+				alert("Terminó tu turno prro");
+				resetSelect();
+				animar=0;
+				cuyo=1;
+			}
+		} 
+  }
   else
   {
 	resetSelect();
